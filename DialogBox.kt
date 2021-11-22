599163c1f3731639f86d15684d6f11b472270ef9
Pedrogithub1 <75342808+Pedrogithub1@users.noreply.github.com>
2021-11-22 09:53:27 -0600

    Add files via upload

---
+++ DialogBox.kt
@@ -0,0 +1,36 @@
+package mx.pemg.guesswho
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.text.InputType
+import android.view.View
+import android.view.Window
+import android.widget.EditText
+import android.widget.ImageView
+
+class DialogBox(contexto: Context?, private val interfaz: EndDialogBox, val msg: String) {
+    interface EndDialogBox {
+        fun ResultDialogBox(mensaje: String?)
+    }
+
+    init {
+        val dialogo = Dialog(contexto!!)
+        dialogo.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialogo.setCancelable(false)
+        dialogo.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialogo.setContentView(R.layout.dialog_box)
+        val mensaje = dialogo.findViewById<View>(R.id.mensaje) as EditText
+        val aceptar = dialogo.findViewById<View>(R.id.aceptar) as ImageView
+        val cancelar = dialogo.findViewById<View>(R.id.cancelar) as ImageView
+        mensaje.inputType = InputType.TYPE_CLASS_TEXT
+        mensaje.setText(msg)
+        aceptar.setOnClickListener {
+            interfaz.ResultDialogBox(mensaje.text.toString())
+            dialogo.dismiss()
+        }
+        cancelar.setOnClickListener { dialogo.dismiss() }
+        dialogo.show()
+    }
+}
